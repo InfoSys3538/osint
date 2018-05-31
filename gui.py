37648702432ed1165a9433de09f3a6c3afeca59c
Charles <insfosys3538@gmail.com>
2018-05-31 12:05:50 -0400

    added a button you guys!

--- gui.py
+++ gui.py
@@ -4,7 +4,13 @@
 from Tkinter import *
 
 window = Tk()
+#ya boi, get that window size
+window.geometry('1000x750')
 
-window.title("Welcome to LikeGeeks app")
+window.title("This will be the OSINT dashboard")
+
+btn = Button(window, text="Reddit OSINT")
+
+btn.grid(column=1, row=0)
 
 window.mainloop()

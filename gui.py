456301ebb326a7925906e2bb38f256944c85ff5c
Charles <insfosys3538@gmail.com>
2018-05-31 13:23:51 -0400

    added reddit and twitter buttons

--- gui.py
+++ gui.py
@@ -4,13 +4,19 @@
 from Tkinter import *
 
 window = Tk()
+
 #ya boi, get that window size
 window.geometry('1000x750')
 
 window.title("This will be the OSINT dashboard")
 
-btn = Button(window, text="Reddit OSINT")
+#btn stuff
+#first reddit
+reddit_btn = Button(window, text="Reddit OSINT", bg='#FF4500')
+reddit_btn.grid(row=0, column=0)
+#then twitter
+twitter_btn = Button(window, text="Twitter OSINT", bg='#0084b4')
+twitter_btn(row=1, column=0)
 
-btn.grid(column=1, row=0)
 
 window.mainloop()

cccc81301a6a9d62ca81820dcf75222de757d5f2
Charles <insfosys3538@gmail.com>
2018-06-02 12:43:12 -0400

    messgin with buttons to create windows in order to run the scans

--- gui.py
+++ gui.py
@@ -11,12 +11,31 @@ window = Tk()
 
 window.title("This will be the OSINT dashboard")
 
+#eventually the functions will run the osint
 def clicked_redd():
-    messagebox.showinfo('REDDIT OSINT', 'osinting the reddit')
+    #messagebox.showinfo('REDDIT OSINT', 'osinting the reddit')
+    reddit=Toplevel(bg='#FF4500')
+    #reddit.geometry('500x350')
+    reddit.title("REDDIT OSINT")
+    redlbl=Label(reddit, text="osinting the reddit", bg='#FF4500', font=(20), height=10, width=25)
+    redlbl.grid(row=0, column=0, columnspan=3)
+
 def clicked_twit():
-    messagebox.showinfo('TWITTER OSINT', 'osinting the twitter')
+    #messagebox.showinfo('TWITTER OSINT', 'osinting the twitter')
+    twitter=Toplevel(bg='#0084b4')
+    #twitter.geometry('500x350')
+    twitter.title("TWITTER OSINT")
+    twitlbl=Label(twitter, text="osinting the twitter", bg='#0084b4', font=(20), height=10, width=25)
+    twitlbl.grid(row=0, column=0, columnspan=3)
+
 def clicked_face():
-    messagebox.showinfo('FACEBOOK OSINT', 'osinting the facebook')
+    #messagebox.showinfo('FACEBOOK OSINT', 'osinting the facebook')
+    facebook=Toplevel(bg='#3b5998')
+    #facebook.geometry('500x350')
+    facebook.title("FACEBOOK OSINT")
+    facelbl=Label(facebook, text="osinting the facebook", bg='#3b5998', font=(20), height=10, width=25)
+    facelbl.grid(row=0, column=0, columnspan=3)
+
 
 
 lbl=Label(window, text="Select the platform to search")

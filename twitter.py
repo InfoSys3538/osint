ac4d485a406998d404b3859039d9d48ea27e0023
isfosys3538 <mk4cf@mail.umsl.edu>
2018-06-04 12:50:12 -0500

    A working function for the twitter api

---
+++ twitter.py
@@ -0,0 +1,39 @@
+import json
+import tweepy
+from tweepy import OAuthHandler
+
+define twitter():
+
+	try :
+
+		consumer_key = input("Enter consumer key: ")
+		consumer_secret = input("Enter consumer secret: ")
+		access_token = input("Enter access token: ")
+		access_secret = input("Enter access token secret: ")
+
+		#connect with twitter api
+		auth = OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_secret)
+
+		api = tweepy.API(auth)
+		user = api.get_user('2018Osint')
+
+		print ("Processing...")
+
+	except tweepy.TweepError:
+		raise SystemExit("Error. Failed to connect to Twitter.")
+
+	lat = input("Enter latitude: ")
+	long = input("Enter longitude: ")
+	r = input("Enter search radius in km: ")
+	geo = "{},{},{}km".format(lat, long, r)
+
+	count = 1
+	geo_tweets = [status for status in tweepy.Cursor(api.search, geocode=geo).items(100)]
+	for item in geo_tweets:
+		print (count)
+		print (item.text)
+		count += 1
+		#Can print other information and format or write to file as needed
+
+
